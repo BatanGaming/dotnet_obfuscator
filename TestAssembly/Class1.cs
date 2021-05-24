911181981c253c057cd8297d9b730ed14a93f4e3
Pavel Ryabinin <rpk9810@gmail.com>
2021-05-24 13:19:40 +0500

    Test

--- TestAssembly/Class1.cs
+++ TestAssembly/Class1.cs
@@ -1,34 +1,17 @@
-﻿using System.Linq;
-using System.Numerics;
+﻿using System;
 
 namespace TestAssembly
 {
-    public class Class1
+    public class Class1<T> where T: new()
     {
-        private int _a;
+        public T Field;
 
-        public Class1(int a) {
-            _a = a;
-        }
-        
-        
-        
-        public static BigInteger Factorial(int n) {
-            BigInteger result = 1;
-            for (var i = 2; i <= n; ++i) {
-                result *= i;
-            }
-
-            return result;
-        }
-        
-        public BigInteger SumFactorial(int n) {
-            var list = Enumerable.Range(1, n);
-            return list.Aggregate<int, BigInteger>(0, (current, i) => current + Factorial(i));
+        public Class1() {
+            Field = new T();
         }
 
-        public virtual int get() {
-            return _a;
+        public T CreateNew() {
+            return new T();
         }
     }
 }
--- TestAssembly/Program.cs
+++ TestAssembly/Program.cs
@@ -1,13 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 namespace TestAssembly
 {
     public static class Program
     {
         public static void Main(string[] args) {
-            var c = new Class2(2,3);
-            Console.WriteLine(c.get());
-            Console.WriteLine(c.SumFactorial(10));
+            var a = new List<int>();
+            var b = new List<string>();
+            a.Add(1);
+            Console.WriteLine(a[0]);
+            b.Add("123");
+            Console.WriteLine(b[0]);
+            var c = new Class1<List<int>>();
+            c.Field.Add(2);
+            Console.WriteLine(c.Field[0]);
+            var d = c.CreateNew();
+            d.Add(3);
+            Console.WriteLine(d[0]);
         }
     }
 }
